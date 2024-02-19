abc333ae86887305840cd8e3f75b7c321fba2fca
Yury Gorshenin <mipt.vi002@gmail.com>
2024-02-19 16:53:15 +0300

    Pack of configs.

---
+++ custom.el
@@ -0,0 +1,52 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#FBF7EF" "#F23749" "#819500" "#AC8300" "#2B90D8" "#DD459D" "#259D94" "#657377"])
+ '(custom-safe-themes
+   '("81f53ee9ddd3f8559f94c127c9327d578e264c574cda7c6d9daddaec226f87bb" "9013233028d9798f901e5e8efb31841c24c12444d3b6e92580080505d56fd392" "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c" "c5878086e65614424a84ad5c758b07e9edcf4c513e08a1c5b1533f313d1b17f1" default))
+ '(exwm-floating-border-color "#8FAAAB")
+ '(fci-rule-color "#98A8A8")
+ '(highlight-tail-colors
+   ((("#eeedd7" "#f1f1e1" "green")
+     . 0)
+    (("#e5eee5" "#e8f4f0" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#FBF7EF" "#2B90D8"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#FBF7EF" "#819500"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#FBF7EF" "#8FAAAB"))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(objed-cursor-color "#F23749")
+ '(pdf-view-midnight-colors (cons "#657377" "#FBF7EF"))
+ '(rustic-ansi-faces
+   ["#FBF7EF" "#F23749" "#819500" "#AC8300" "#2B90D8" "#DD459D" "#259D94" "#657377"])
+ '(vc-annotate-background "#FBF7EF")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#819500")
+    (cons 40 "#8f8f00")
+    (cons 60 "#9d8900")
+    (cons 80 "#AC8300")
+    (cons 100 "#b97900")
+    (cons 120 "#c76e00")
+    (cons 140 "#D56500")
+    (cons 160 "#d75a34")
+    (cons 180 "#da4f68")
+    (cons 200 "#DD459D")
+    (cons 220 "#e44081")
+    (cons 240 "#eb3b65")
+    (cons 260 "#F23749")
+    (cons 280 "#d95361")
+    (cons 300 "#c07079")
+    (cons 320 "#a78d92")
+    (cons 340 "#98A8A8")
+    (cons 360 "#98A8A8")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
